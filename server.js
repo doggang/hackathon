f8bd09351f89500dabea62ca1dcb49ef979aa856
mythpoy <okmlnsunok@gmail.com>
2022-10-27 23:36:07 +0900

    바꿈

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.listen(80, (req, res) => {
   console.log('server on! http://localhost:'+80);
 })
 
-// mongoose.connect(
-//   process.env.DB_URL,
-//   {
-//      useNewUrlParser: true,
-//   }
-// )
+mongoose.connect(
+  'mongodb+srv://okmlnsunok:jyp1234@cluster0.i8mgpkg.mongodb.net/hk?retryWrites=true&w=majority',
+  {
+     useNewUrlParser: true,
+  }
+)
 
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-//   console.log("Connected successfully");
-// });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
